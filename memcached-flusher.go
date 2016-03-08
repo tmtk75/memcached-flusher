147227817b2b0ab0f2f33971695ec3311fe7136c
Francisco A. Lozano <flozano@gmail.com>
2016-03-08 18:26:18 +0100

    Initial commit

---
+++ memcached-flusher.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/bradfitz/gomemcache/memcache"
+import "time"
+import "log"
+import "os"
+
+func main() {
+	mc := memcache.New(os.Args[1:])
+	for {
+		err := mc.FlushAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Duration(20) * time.Millisecond)
+	}
+}
